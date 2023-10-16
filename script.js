367a3770c26ae40593660f6f4eda6c7e98a66e51
IgorBal2023 <protuberanec2023@gmail.com>
2023-10-16 18:39:04 +0300

    add comment and keyFunction

--- script.js
+++ script.js
@@ -6,20 +6,33 @@ const dictionary = {
   why: "чому",
   which: "який",
 };
-const key = Object.keys(dictionary);
-const randomKey = key[Math.floor(Math.random() * key.length)];
-const keyWord = document.querySelector(".keyWordAll");
+// функція, яка рандомно викликає та виводить ключі
+const keyFunction = () => {
+  const key = Object.keys(dictionary);
+  const randomKey = key[Math.floor(Math.random() * key.length)];
+  const keyWord = document.querySelector(".keyWordLi");
 
-keyWord.insertAdjacentHTML("afterbegin", `<p>${randomKey}</p>`);
+  keyWord.insertAdjacentHTML("afterbegin", `<li>${randomKey}</li>`);
+};
+//start button
+const start = document.getElementById("start");
+start.addEventListener(`click`, function () {
+  keyFunction();  
+});
 
+// DOM перетворення
 const answer = document.getElementById("myInput");
 const button = document.getElementById("myButton");
-const valueWord = document.querySelector(`.valueWordAll`);
+const valueWord = document.querySelector(`.valueWord`);
 
+// прив'язка до кнопки та вивод значення
 button.addEventListener(`click`, function () {
-    const userAnswer = answer.value.trim();
-  if (userAnswer !== ""){
-    valueWord.insertAdjacentHTML("afterbegin", `<p>${userAnswer}</p>`);
-    answer.value = "";
-}
+  const userAnswer = answer.value.trim();
+  if (userAnswer !== "") {
+    valueWord.insertAdjacentHTML("afterbegin", `<li>${userAnswer}</li>`);
+    answer.value = ""; //очищення ввода
+    keyFunction();
+  } else {
+    alert(`Введіть відповідь`);
+  }
 });
