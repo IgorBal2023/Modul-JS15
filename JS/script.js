9c1cc29f9406f69d8837ae2db69cbdfc60df8911
IgorBal2023 <protuberanec2023@gmail.com>
2023-10-23 20:24:02 +0300

    вивод невірних відповідей

--- JS/script.js
+++ JS/script.js
@@ -2,68 +2,96 @@ import { data } from "./dictionary.js";
 
 let randomKey;
 // DOM перетворення
-
 const start = document.getElementById("start");
 const answer = document.getElementById("myInput");
 const button = document.getElementById("myButton");
 const keyWord = document.querySelector(".keyWordLi");
-const valueWord = document.querySelector(`.valueWord`);
+const valueWord = document.querySelector(`.firstString`);
 const score = document.querySelector(`.score`);
 score.innerText = 0;
 let number = 1;
 let dictionaryNumber = data[number]; // словники
 const level = document.querySelector(`.level`);
 level.innerText = `Level  `;
+const errorWords = [];
+const errorArrow = document.querySelector(`.errorWordsUl`);
 
 // функція, яка рандомно викликає та виводить ключі
 const keyFunction = () => {
-    const key = Object.keys(dictionaryNumber);
-    randomKey = key[Math.floor(Math.random() * key.length)];
+  const key = Object.keys(dictionaryNumber);
+  randomKey = key[Math.floor(Math.random() * key.length)];
 
-    level.innerText = `Level  ${number}`;
-    keyWord.insertAdjacentHTML("afterbegin", `<li>${randomKey}</li>`);
+  level.innerText = `Level  ${number}`;
+  keyWord.insertAdjacentHTML("afterbegin", `<li>${randomKey}</li>`);
 };
+
 // вивод значення
 function insertAnswer() {
   const userAnswer = answer.value.trim();
-  if(keyWord.textContent.trim() === ""){
+  if (keyWord.textContent.trim() === "") {
     answer.value = "";
-        alert("Натисніть 'Start'");
-  }
-  else if (userAnswer !== "") {
-    valueWord.insertAdjacentHTML("beforeend", `<li>${userAnswer}</li>`);
+    alert("Натисніть 'Start'");
+  } else if (userAnswer !== "") {
     answer.value = ""; //очищення ввода
-    // порівняння відповіді та додавання очок
-    if (
-      userAnswer.toLowerCase() === dictionaryNumber[randomKey].toLowerCase()
-    ) {
-      score.innerText = String(parseInt(score.innerText) + 1);
-      if (parseInt(score.innerText) >= 10) {
-        number++;
-        score.innerText = 0;
-        dictionaryNumber = data[number];
-        clearOutput();
-      }
-    } else {
-      score.innerText = String(parseInt(score.innerText) - 1);
-    }
-    keyFunction();
+    coinAnswer(userAnswer);
   } else {
     alert(`Введіть відповідь`);
   }
 }
+
+// порівняння відповіді та додавання очок
+function coinAnswer(userAnswer) {
+  if (userAnswer.toLowerCase() === dictionaryNumber[randomKey].toLowerCase()) {
+    score.innerText = String(parseInt(score.innerText) + 1);
+    valueWord.insertAdjacentHTML(
+      "afterend",
+      `<li class="correctAnswer">${userAnswer}</li>`
+    );
+    if (parseInt(score.innerText) >= 10) {
+      number++;
+      score.innerText = 0;
+      dictionaryNumber = data[number];
+      clearOutput();
+    }
+  } else {
+    score.innerText = String(parseInt(score.innerText) - 1);
+    valueWord.insertAdjacentHTML(
+      "afterend",
+      `<li class="inCorrectAnswer">${userAnswer}</li>`
+    );
+    errorWords.push(randomKey); //запис невірних відповідей
+    errorArrowFoo();
+  }
+  keyFunction();
+}
+
 //start button прив'язка до кнопки
 start.addEventListener(`click`, keyFunction, { once: true });
 button.addEventListener(`click`, insertAnswer);
-answer.addEventListener("keydown", function (event) {
+answer.addEventListener("keyup", function (event) {
   if (event.key === "Enter") {
     button.click();
   }
 });
+
 // очищення на наступному рівні
 function clearOutput() {
   const keyWord = document.querySelector(".keyWordLi");
-  const valueWord = document.querySelector(".valueWord");
+  const valueWords = document.querySelectorAll(".valueWord li");
   keyWord.innerHTML = "";
-  valueWord.innerHTML = `<li class="firstString"> --- </li>`; // вирівнення строк
+  for (let i = 1; i < valueWords.length; i++) {
+    valueWords[i].remove();
+  }
+}
+
+// фільтрація та виведення невірних відповідей
+function errorArrowFoo() {
+  const errorWordsFilter = [...new Set(errorWords)];
+  errorArrow.innerHTML = "";
+  errorWordsFilter.forEach((item) => {
+    const errorWordsLi = document.createElement("li");
+    errorWordsLi.textContent = item;
+    errorWordsLi.style.color = "red";
+    errorArrow.appendChild(errorWordsLi);
+  });
 }
