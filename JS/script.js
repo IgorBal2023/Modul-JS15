6f69569b460df3572e46ea93bcf423e83964077d
IgorBal2023 <protuberanec2023@gmail.com>
2023-10-23 14:13:40 +0300

    перевірка на пусте значення

--- JS/script.js
+++ JS/script.js
@@ -6,6 +6,7 @@ let randomKey;
 const start = document.getElementById("start");
 const answer = document.getElementById("myInput");
 const button = document.getElementById("myButton");
+const keyWord = document.querySelector(".keyWordLi");
 const valueWord = document.querySelector(`.valueWord`);
 const score = document.querySelector(`.score`);
 score.innerText = 0;
@@ -14,21 +15,22 @@ let dictionaryNumber = data[number]; // словники
 const level = document.querySelector(`.level`);
 level.innerText = `Level  `;
 
-
 // функція, яка рандомно викликає та виводить ключі
 const keyFunction = () => {
-  const key = Object.keys(dictionaryNumber);
-  randomKey = key[Math.floor(Math.random() * key.length)];
-  const keyWord = document.querySelector(".keyWordLi");
+    const key = Object.keys(dictionaryNumber);
+    randomKey = key[Math.floor(Math.random() * key.length)];
 
-  level.innerText = `Level  ${number}`;
-  keyWord.insertAdjacentHTML("afterbegin", `<li>${randomKey}</li>`);
+    level.innerText = `Level  ${number}`;
+    keyWord.insertAdjacentHTML("afterbegin", `<li>${randomKey}</li>`);
 };
-
 // вивод значення
 function insertAnswer() {
   const userAnswer = answer.value.trim();
-  if (userAnswer !== "") {
+  if(keyWord.textContent.trim() === ""){
+    answer.value = "";
+        alert("Натисніть 'Start'");
+  }
+  else if (userAnswer !== "") {
     valueWord.insertAdjacentHTML("beforeend", `<li>${userAnswer}</li>`);
     answer.value = ""; //очищення ввода
     // порівняння відповіді та додавання очок
@@ -53,7 +55,7 @@ function insertAnswer() {
 //start button прив'язка до кнопки
 start.addEventListener(`click`, keyFunction, { once: true });
 button.addEventListener(`click`, insertAnswer);
-answer.addEventListener(`keydown`, function (event) {
+answer.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
     button.click();
   }
@@ -63,5 +65,5 @@ function clearOutput() {
   const keyWord = document.querySelector(".keyWordLi");
   const valueWord = document.querySelector(".valueWord");
   keyWord.innerHTML = "";
-  valueWord.innerHTML = `<li class="firstString"> --- </li>`;// вирівнення строк
-  }
+  valueWord.innerHTML = `<li class="firstString"> --- </li>`; // вирівнення строк
+}
